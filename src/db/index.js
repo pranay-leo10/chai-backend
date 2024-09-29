d32f2f02f63e5c3817e603c3c1a98874acf7600b
pranay-leo10 <pranaygupta1608@gmail.com>
2024-09-29 14:55:13 +0530

    Update gitignore

--- src/db/index.js
+++ src/db/index.js
@@ -1,15 +1,17 @@
-import mongoose from "mongoose";
+import mongoose  from "mongoose";
 import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv"
 
+dotenv.config({ path: './.env' });
 
 const connectDB = async ()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
-        console.log(`MongoDB connection error`, error)
+        console.log("MongoDB connection error", error)
         process.exit(1)
     }
-}
+} 
 
 export default connectDB
